21365be6cedf04a37c7b05e2903eb90e5f7a25bc
Henry A Baum <henry.baum46@myhunter.cuny.edu>
2021-06-30 01:42:44 -0400

    Added User Profile Route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home.js';
+import UserProfile from './components/UserProfile.js';
 import './App.css';
 
 
@@ -8,16 +9,22 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      accountBalance:14568.27
+      accountBalance:14568.27,
+      currentUser: {
+        userName: 'Bobby',
+        memberSince: '01/01/1990'
+      }
     }
   };
 
   render() {
     const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
+    const UserProfileComponent = () => (<UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince}/>);
     return (
       <Router>
         <Switch>
           <Route exact path="/" component={HomeComponent}/>
+          <Route exact path={"/user/" + this.state.currentUser.userName} render={UserProfileComponent}/>
         </Switch>
       </Router>
     );
